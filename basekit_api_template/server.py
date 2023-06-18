b3fb556fe5f56ca5bd525dee1595d6b477bb254b
marcorsouza <marco.rsouza@gmail.com>
2023-06-18 03:50:05 -0300

    commit

--- basekit_api_template/server.py
+++ basekit_api_template/server.py
@@ -2,7 +2,7 @@ import inspect
 import os
 from flask import Flask
 from basekit_api_template.core_lib.route_loader import load_routes
-from basekit_api_template.core_lib.swagger_config import swagger_init_app
+from basekit_core_lib.config.swagger_config import swagger_init_app
 from basekit_api_template.core_lib.app_container import AppContainer
 from basekit_core_lib.server_base import ServerBase
 
@@ -16,4 +16,4 @@ class Server(ServerBase):
         load_routes(self.blue_print, container)
         
     def swagger_init_app(self):
-        swagger_init_app(self.app)
+        swagger_init_app(self.app,'BKT Blog',1,'Este é um blog desenvolvimento baseado no BaseKit Template!')
