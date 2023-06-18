30f7d0f00e1353a5387cc456827dfdcc3cc6cc64
marcorsouza <marco.rsouza@gmail.com>
2023-06-18 03:24:49 -0300

    commit

--- basekit_api_template/server.py
+++ basekit_api_template/server.py
@@ -1,48 +1,19 @@
 import inspect
 import os
-from flask import Blueprint, Flask
-from flask_jwt_extended import JWTManager
+from flask import Flask
 from basekit_api_template.core_lib.route_loader import load_routes
-from basekit_core_lib.config.helpers import db, ma, config
 from basekit_api_template.core_lib.swagger_config import swagger_init_app
-from basekit_core_lib.api.middlewares.auth_middleware import authenticate
 from basekit_api_template.core_lib.app_container import AppContainer
+from basekit_core_lib.server_base import ServerBase
 
 container = AppContainer()  
 
-class Server:
+class Server(ServerBase):
     def __init__(self, app: Flask) -> None:
-        self.app = app
-        self.blue_print = Blueprint('api', __name__, url_prefix='/api')           
-        self.config =config
-        self.app.config.from_object(self.config)
-        
-        self.app.secret_key = self.config.SECRET_KEY
-        self.app.config['JWT_SECRET_KEY'] = self.config.SECRET_KEY # Defina sua chave secreta
-        JWTManager(self.app)
-                     
-        self.configure_routes()
-        self.app.register_blueprint(self.blue_print)
-        
-        db.init_app(self.app)
-        ma.init_app(self.app)
-        swagger_init_app(self.app)
-        
-        self.configure_autenticate_routes()
-        
-        super().__init__()
+        super().__init__(app)
         
     def configure_routes(self):            
         load_routes(self.blue_print, container)
         
-    def configure_autenticate_routes(self,):
-        for route in self.app.url_map.iter_rules():
-            if route.rule.startswith('/api/') and not route.rule.startswith('/api/auth/') and not 'recovery_password' in route.rule:
-                self.app.view_functions[route.endpoint] = authenticate(self.app.view_functions[route.endpoint])
-            
-    def run(self, ):
-        self.app.run(
-            port=self.config.PORT,
-            debug=self.config.DEBUG,
-            host=self.config.HOST
-        )
+    def swagger_init_app(self):
+        swagger_init_app(self.app)
